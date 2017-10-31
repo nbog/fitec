329a9765bedef47f0566f93501bca8c0665cadaa
nbog <33249231+nbog@users.noreply.github.com>
2017-10-31 14:53:41 +0100

    simplification test

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -34,10 +34,8 @@ class FoobarQixTestCase(unittest.TestCase):
     def test_retrurn_FooBar_when_number_is_divisible_by_3_and_5(self):
         self.assertEqual(foobarqix(15), "FooBar")
 
-    def test_retrurn_1_when_number_is_1(self):
+    def test_return_number_as_a_string_otherwise(self):
         self.assertEqual(foobarqix(1),"1")
-
-    def test_retrurn_2_when_number_is_2(self):
         self.assertEqual(foobarqix(2),"2")
 
 
