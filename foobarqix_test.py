cd545b62c19e96f9d58e7203e5ffd58562c7cb1a
nbog <33249231+nbog@users.noreply.github.com>
2017-10-31 12:08:54 +0100

    ajout divisibilité par 7

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -8,9 +8,10 @@ def is_divisible(number, divisor):
 def foobarqix(number):
     if is_divisible(number, 3):
         return "Foo"
-    else:
+    if is_divisible(number, 5):
         return "Bar"
-
+    if is_divisible(number, 7):
+        return "Qix"
 
 class FoobarQixTestCase(unittest.TestCase):
     def test_retrurn_Foo_when_number_is_divisible_by_3(self):
@@ -18,6 +19,9 @@ class FoobarQixTestCase(unittest.TestCase):
 
     def test_retrurn_Bar_when_number_is_divisible_by_5(self):
         self.assertEqual(foobarqix(5), "Bar")
+
+    def test_retrurn_Qix_when_number_is_divisible_by_7(self):
+        self.assertEqual(foobarqix(7), "Qix")
 
 
 unittest.main()
