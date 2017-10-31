b76d5afc78118089ba092ff958946c7cf80ee72e
nbog <33249231+nbog@users.noreply.github.com>
2017-10-31 15:27:15 +0100

    boucle 0/100

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -1,22 +1,5 @@
 import unittest
-
-
-def is_divisible(number, divisor):
-    return number % divisor == 0
-
-
-def foobarqix(number):
-    result = ""
-    if is_divisible(number, 3):
-        result += "Foo"
-    if is_divisible(number, 5):
-        result += "Bar"
-    if is_divisible(number, 7):
-        result += "Qix"
-    if result != "":
-        return result
-    else:
-        return str(number)
+from foobarqix import foobarqix
 
 
 class FoobarQixTestCase(unittest.TestCase):
---
+++ main.py
@@ -0,0 +1,4 @@
+from foobarqix import foobarqix
+
+for number in range(0,100):
+    print(number, foobarqix(number))
